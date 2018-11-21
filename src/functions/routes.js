5c95d7d73fecc1c4acfd359ecb3b4890079f1003
hilotter <hilotter3@gmail.com>
2018-11-22 00:56:43 +0900

    add cache-control
    https://firebase.google.com/docs/hosting/functions?hl=ja#set_cache_control

--- src/functions/routes.js
+++ src/functions/routes.js
@@ -1,6 +1,10 @@
 const routes = require('next-routes')();
 
 routes
+  .add('/', 'index')
+  .add('/privacy', 'privacy')
+  .add('/about', 'about')
+  .add('/publish', 'publish')
   .add('/detail/:tokenId', 'detail');
 
 module.exports = routes;
